961fcfea35d7307ce14614a13833f86998e0fe5d
wancheng <wancheng.com.cn@gmail.com>
2013-09-13 14:27:13 +0800

    employee data

---
+++ test.py
@@ -0,0 +1,54 @@
+#!/bin/sh
+# coding=utf-8  
+import xlrd
+import MySQLdb
+import sys
+
+def open_excel(file="file.xls"):
+	reload(sys)
+	print sys.setdefaultencoding("utf-8")
+	try:
+		print("================ get data ===============")
+		print(file)
+		data = xlrd.open_workbook(file,encoding_override="gbk")
+		return data
+	except Exception,e:
+		print str(e)
+
+def excel_table_byindex(file="file.xls",colnameindex=0,by_index=0):
+	conn = MySQLdb.connect(host='localhost',user='root',passwd='rootroot',db='blog')
+	cursor = conn.cursor()
+	# cursor.execute""()
+	data = open_excel(file)
+	table = data.sheets()[by_index]
+	nrows = table.nrows
+	ncols = table.ncols
+	print("==nrows:"+str(nrows))
+	print("==ncols:"+str(ncols))
+	colnames = table.row_values(colnameindex)
+
+	for rownum in range(1,nrows):
+		row = table.row_values(rownum)
+		if row:
+			app = []
+			for i in range(len(colnames)):
+				if i==0:
+					app.append(row[i])
+				elif i==1:
+					s=1
+					if row[i]=='F':
+						s=0
+					app.append(s)
+				else:
+					app.append(row[i])
+			print "app length:",len(app)
+			sql="INSERT INTO employee (employeeid,sex,ename,efname,cname) VALUES ('%s',%d,'%s','%s','%s')"%(int(app[0]),int(app[1]),str(app[2]),str(app[3]),str(app[4]))
+			print sql
+			cursor.execute(sql)
+	cursor.close()
+
+def main():
+	excel_table_byindex(file="dao.xlsx")
+
+if __name__ == "__main__":
+	main()
