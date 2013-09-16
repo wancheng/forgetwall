e4b1df1429b25ed5bc3cd6f404f8ce05dfb30230
wancheng <wancheng.com.cn@gmail.com>
2013-09-16 15:56:25 +0800

    binding result

--- handler/weixin/binding.py
+++ handler/weixin/binding.py
@@ -2,4 +2,19 @@ from handler.base import BaseHandler
 
 class Binding(BaseHandler):
 	def get(self):
-		self.render("weixin/binding.html")
+		weixinid = self.get_argument("weixinid")
+		print weixinid
+		self.render("weixin/binding.html",weixinid=weixinid)
+
+	def post(self):
+		employeeid = self.get_argument("employeeid")
+		ename = self.get_argument("ename")
+		weixinid = self.get_argument("weixinid")
+		print employeeid
+		print ename
+		employees = self.db.query("SELECT * FROM employee WHERE employeeid = %s AND ename = %s",employeeid,ename)
+		if not employees:
+			self.render("weixin/info.html",info="error!",css="error")
+		else:
+			self.db.execute("UPDATE weixin_user SET employeeid = %s WHERE weixinid = %s",employeeid,weixinid)
+			self.render("weixin/info.html",info="success!",css="success")
