6fa26e7659da25f307dc4af3f7902210a65e41d2
wancheng <wancheng.com.cn@gmail.com>
2013-10-12 16:19:16 +0800

    resume

--- handler/weixin/job.py
+++ handler/weixin/job.py
@@ -4,17 +4,22 @@ import logging
 class JobHandler(BaseHandler):
 	def get(self):
 		method = self.get_argument("method")
+		weixinid = self.get_argument("weixinid")
 		if method == "list":
 			jobs = self.db.query("SELECT * FROM job ORDER BY ID LIMIT 10")
-			url = self.request.uri[1:-12]
-			logging.error(url)
-			self.render("weixin/jobs.htm",jobs=jobs)
+			self.render("weixin/jobs.htm",jobs=jobs,weixinid=weixinid)
 		elif method == "info":
 			id = self.get_argument("id")
 			job = self.db.get("SELECT * FROM job WHERE id = %s",int(id))
-			self.render("weixin/job.htm",job=job)
+			self.render("weixin/job.htm",job=job,weixinid=weixinid)
 		else:
 			self.write("ERROR")
+	def post(self):
+		weixinid = self.get_argument("weixinid")
+		resume = self.get_argument("resume")
+		logging.error(weixinid)
+		logging.error(resume)
+		self.write("success")
 
 	"""
 		jobid = self.get_argument("id")
